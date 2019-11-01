2bb4e7269c250c56dc954aaf7dfc56658c4ae1ce
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2019-11-01 11:23:58 +0200

    Простая игра - угадай число

--- src/jptvr18lesson1/JPTVR18Lesson1.java
+++ src/jptvr18lesson1/JPTVR18Lesson1.java
@@ -5,6 +5,8 @@
  */
 package jptvr18lesson1;
 
+import myclasses.App;
+
 /**
  *
  * @author Melnikov
@@ -15,7 +17,8 @@ public class JPTVR18Lesson1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        App app = new App();
+        app.run();
     }
     
 }
---
+++ src/myclasses/App.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclasses;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class App {
+    public void run(){
+        System.out.println("Привеееет, группа JPTVR18!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Напишите свое имя латинскими буквами: ");
+        String myName = scanner.nextLine();
+        System.out.println("Hi, "+myName);
+        Random random = new Random();
+        int randomNumber = random.nextInt(5);
+        System.out.println("Задумано число от 0 до 4. Угадай?");
+        int userNumber = scanner.nextInt();
+        if(userNumber == randomNumber){
+            System.out.println("Ты угадал, молодец!");
+        }else{
+            System.out.println("Не повезло :(, задумано число: "+randomNumber);
+        }
+    }
+}
